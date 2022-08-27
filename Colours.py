d12cd7adcefe5e8239d4d18cbebf37fded501be7
Suchir-Phoenix-King <88494177+Suchir-Phoenix-King@users.noreply.github.com>
2022-08-27 15:15:24 +0530

    Add files via upload

---
+++ Colours.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 17:39:31 2022
+
+@author: PC_RC
+"""
+
+from tkinter import*
+
+root = Tk()
+root.title("Random Colour")
+root.geometry("600x400")
+
+colours = {'colours':'cyan','blue','navy','dark orange','gold','HotPink1','black','maroon','LightBlue1','azure', 'mint cream', 'magenta3', 'SlateBlue1', 'spring green', 'linen', 'orange'}
